8ac67cad0c67b4426b775a3418323ab77ee10b20
rik12 <nimishagarwal1993@gmail.com>
2024-05-23 20:59:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function initMap() {
+    var location = { lat: -34.397, lng: 150.644 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for subscribing!');
+});
